846966302df6f57172090604d9b1cb440f20915a
Jerome Chong <jeromechong229@gmail.com>
2025-07-12 22:44:36 +0100

    add sqlx integration

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ use actix_web::dev::Server;
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn _greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
@@ -11,11 +17,14 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            // .route("/", web::get().to(greet))
-            // .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/health_check", web::get().to(health_check))
     })
     .listen(listener)?
