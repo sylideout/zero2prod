a0cb372e54fafad5c75ddd8eb3c27268ccd87062
Jerome Chong <jeromechong229@gmail.com>
2025-07-13 13:18:59 +0100

    add config file

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,16 @@
 use actix_web::dev::Server;
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use actix_web::{App, HttpServer, web};
 use std::net::TcpListener;
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn _greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/subscriptions", web::post().to(subscribe))
-            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
+            .route("/health_check", web::get().to(routes::health_check))
     })
     .listen(listener)?
     .run();
